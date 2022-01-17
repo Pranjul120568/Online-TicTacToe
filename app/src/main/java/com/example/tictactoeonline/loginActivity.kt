48414c0255c600cfda2833c0f534f220207036b2
Pranjul120568 <pranjuldwivedi120568@gmail.com>
2022-01-17 17:02:23 +0530

    Layout Implemented

--- app/src/main/java/com/example/tictactoeonline/loginActivity.kt
+++ app/src/main/java/com/example/tictactoeonline/loginActivity.kt
@@ -1,20 +1,13 @@
 package com.example.tictactoeonline
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.google.firebase.auth.FirebaseAuth
 
-class MainActivity : AppCompatActivity() {
+class loginActivity : AppCompatActivity() {
     private var auth: FirebaseAuth? =null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        if(auth==null){
-            startActivity(Intent(this,loginActivity::class.java))
-        }else{
-
-        }
-
+        setContentView(R.layout.activity_login)
     }
 }
